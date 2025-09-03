b800b0738f225b8ca6b58b2cae65b9dde11b038b
mafb <marcofbraga@gmail.com>
2025-09-03 14:27:26 -0300

    Atualizando calculadora e serviço API

---
+++ src/services/apiService.ts
@@ -0,0 +1,17 @@
+const API_URL = process.env.PUBLIC_API_URL || 'http://localhost:3001';
+
+export async function submit(formData: FormData): Promise<boolean> {
+    try {
+        const response = await fetch(API_URL + '/contact', {
+            method: 'POST',
+            body: formData
+        });
+
+        const result = await response.json();
+        return result.success;
+
+    } catch (error) {
+        console.error('Erro ao enviar para API', error);
+        return false;
+    }
+}
